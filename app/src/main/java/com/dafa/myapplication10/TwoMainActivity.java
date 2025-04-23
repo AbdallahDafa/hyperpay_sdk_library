be55676d72ef01598198d6a8534cfe00e92004f8
AbdallahDafa <abdallah.mahmoud@dafa.sa>
2025-04-23 11:18:10 +0200

    Initial commit

---
+++ app/src/main/java/com/dafa/myapplication10/TwoMainActivity.java
@@ -0,0 +1,14 @@
+package com.dafa.myapplication10;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class TwoMainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_two_main);
+    }
+}
