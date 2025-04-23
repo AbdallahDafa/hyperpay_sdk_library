b26b0ce69815205f80ed1504a12809ebd1cf0284
AbdallahDafa <abdallah.mahmoud@dafa.sa>
2025-04-23 14:52:08 +0200

    1.0.11 publish   * remove .aar files and import paths

--- app/src/main/java/com/dafa/hyperpaysdk/core/TestHyperPay.java
+++ app/src/main/java/com/dafa/hyperpaysdk/core/TestHyperPay.java
@@ -1,5 +1,5 @@
 package com.dafa.hyperpaysdk.core;
-
+//import com.oppwa.mobile.connect.provider.Connect
 public class TestHyperPay {
 
     public static String getName() { return  "i am still in development hyperpay";  }
